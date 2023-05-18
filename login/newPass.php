ee636a6a20ed2d9211dff99f4b211e6b253984a7
Salwa Rafik <salwara37@gmail.com>
2023-05-18 21:22:29 +0300

    updates

--- login/newPass.php
+++ login/newPass.php
@@ -5,60 +5,59 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
-require_once 'dbconfig.php';
 
-require_once 'PHPMailer-master/src/PHPMailer.php';
-require_once 'PHPMailer-master/src/SMTP.php';
-require_once 'PHPMailer-master/src/Exception.php';
+require_once '../dbconfig.php';
 
-$db = connect_pdo();
-
-// Check if the email and token are present in the query string
-if (isset($_GET['email']) && isset($_GET['token'])) {
-    $email = $_GET['email'];
-    $token = $_GET['token'];
-
-    // Check if the token is valid
-    $stmt = $db->prepare("SELECT * FROM users WHERE email = :email AND reset_token = :token");
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':token', $token);
-    $stmt->execute();
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (!$user) {
-        // Invalid token or email address
-        header('Location: login.php');
-        exit;
-    }
-} else {
-    // Email and/or token not present in the query string
-    header('Location: login.php');
-    exit;
+// Connect to the database using PDO
+try {
+    $db = connect_pdo();
+} catch (PDOException $e) {
+    die("Failed to connect to the database: " . $e->getMessage());
 }
 
-// Handle the password reset form submission
-if (isset($_POST['reset'])) {
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
+// Check if the token is valid and not expired
+if (isset($_GET['token'])) {
+    $token = $_GET['token'];
+    $query = "SELECT * FROM password_reset_tokens WHERE token=:token AND expires_at>:now";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':token', $token);
+    $stmt->bindValue(':now', date('Y-m-d H:i:s'));
+    $stmt->execute();
 
-    if ($password === $confirm_password) {
-        // Hash the new password
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    if ($stmt->rowCount() == 1) {
+        $reset_request = $stmt->fetch(PDO::FETCH_ASSOC);
+        $user_id = $reset_request['user_id'];
 
-        // Update the user's password and reset token in the database
-        $stmt = $db->prepare("UPDATE users SET password = :password, reset_token = NULL WHERE email = :email");
-        $stmt->bindParam(':password', $hashed_password);
-        $stmt->bindParam(':email', $email);
-        $stmt->execute();
+        if (isset($_POST['submit'])) {
+            $password = $_POST['password'];
+            $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-        echo 'Password reset successfully.';
+            // Update the user's password in the database
+            $query = "UPDATE users SET password=:password WHERE id=:user_id";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':password', $password_hash);
+            $stmt->bindParam(':user_id', $user_id);
+            $stmt->execute();
 
-        // Redirect the user to the login page
-        header('Location: login.php');
-        exit;
+            // Delete the reset request from the database
+            $query = "DELETE FROM password_reset_tokens WHERE token=:token";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':token', $token);
+            $stmt->execute();
+
+            echo "Your password has been reset. <a href=\"login.php\">Login</a> with your new password.";
+        } else {
+            echo "<form action=\"newPass.php?token=$token\" method=\"post\">";
+            echo "<label for=\"password\">New password:</label>";
+            echo "<input type=\"password\" name=\"password\" id=\"password\" required>";
+            echo "<button type=\"submit\" name=\"submit\">Reset password</button>";
+            echo "</form>";
+        }
     } else {
-        echo 'Passwords do not match.';
+        echo "Invalid or expired token.";
     }
+} else {
+    echo "Token not provided.";
 }
 ?>
 <!DOCTYPE html>
