979b733eff9db44a340885dc037e6c37451a03ed
asmaa_elrefai <eviltour11@gmail.com>
2023-05-19 09:40:49 +0300

    solve debug in update

--- products/productsList.php
+++ products/productsList.php
@@ -1,10 +1,39 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <title>Cafeteria</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  </head>
   <body>
   <?php
-  require_once '../dbconfig.php';
-  require_once '../header.html';
+  include '../dbconfig.php';
   ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="../home/homePage.php">Cafeteria</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="../home/homePage.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../users/usersList.php">Users</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../orders/manualOrder.php">Manual Order</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../checks/checksList.php">Checks</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     
     <div class="container mt-4">
       <h1>Products List</h1>
@@ -21,7 +50,7 @@ $stmt->execute();
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Loop through the data and display it
-echo "<a  href='productAdd.php' class='btn btn-success me-2 ms-auto'>Add Product</a>";
+echo "<a  href='productAdd.php' class='btn btn-success float-end'>Add Product</a>";
 echo "<table class='table table-striped'>";
 echo "<tr><th>Product ID</th><th>Product_Name</th><th>Price</th><th>Image</th><th></th><th>Actions</th></tr>";
 foreach ($data as $row) {
@@ -45,10 +74,10 @@ echo "</tr>";
 echo "</table>";
 ?>
       </div>
-
-  <?php
-  require_once '../footer.html';
-  ?>
+    
+    <footer class="bg-light text-center mt-4 p-3">
+      <p>&copy; Cafeteria. All rights reserved.</p>
+    </footer>
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
